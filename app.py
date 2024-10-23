2e0eb6054794958c40a21302e555c78efdf55454
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-23 19:39:10 -0300

    att

--- app.py
+++ app.py
@@ -5,36 +5,45 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# Inicializando Bootstrap e Moment.js no Flask
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+# Tratamento de erro 404 - Página não encontrada
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+# Tratamento de erro 500 - Erro interno do servidor
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+# Rota principal - Página inicial
 @app.route('/')
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
+# Rota para a página de avaliação
 @app.route('/avaliacao')
 def avaliacao():
     return render_template('avaliacao.html', current_time=datetime.utcnow())
 
+# Rota para a página de identificação
 @app.route('/identificacao')
 def identificacao():
     return render_template('identificacao.html')
 
+# Rota para a página de contexto de requisição
 @app.route('/contextorequisicao')
 def contextorequisicao():
     return render_template('contextorequisicao.html')
 
+# Rota para a página de usuário dinâmico
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+# Executando a aplicação no modo debug
 if __name__ == '__main__':
     app.run(debug=True)
